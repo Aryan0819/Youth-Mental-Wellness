fa036c4b30efcea7edf3961ba26d46ff1294e870
Aryan0819 <aryanrajnc8521@gmail.com>
2025-09-19 20:55:10 +0530

    mood1.py

--- mood1.py
+++ mood1.py
@@ -71,7 +71,7 @@ mood_to_images = {
     "sad": "https://images.unsplash.com/photo-1534043464124-3be32fe00c02?q=80&w=1740&auto=format&fit=crop",
     "anxious": "image/calm 2.jpg",
     "stressed": "https://images.unsplash.com/photo-1549488344-934d402b11e2?q=80&w=1740&auto=format&fit=crop",
-    "lonely": "https://images.unsplash.com/photo-1549488344-934d402b11e2?q=80&w=1740&auto=format&fit=crop",
+    "lonely": "image/loneliness.jpg",
     "grateful": "image/grateful.jpg",
     "energized": "image/eneegized.jpg",
 }
@@ -133,6 +133,7 @@ if st.button("Reflect with AI"):
             st.image(img_url, use_container_width=True)
         else:
             st.write("No image available for this mood.")
+
 
 
 
